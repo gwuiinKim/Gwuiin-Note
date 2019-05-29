ebabbc640fa476b58349df7bc301ccd31b85ae32
gwuiinkim <gwuiinkim@gmail.com>
2019-05-29 22:37:15 +0900

    set up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import GlobalStyle from "./globalStyles";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <GlobalStyle />
+    </div>
+  );
 }
 
 export default App;
